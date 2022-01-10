2a994e2c434973a0a8434ab76b060614b813520f
zohdit <zohdit@usi.ch>
2022-01-10 14:19:48 +0100

    bug fixed

--- DeepHyperion-MNIST/plot_utils.py
+++ DeepHyperion-MNIST/plot_utils.py
@@ -6,30 +6,50 @@ from svgpathtools import svg2paths, wsvg
 import re
 import numpy as np
 import glob
-
+import os
+import pandas as pd
 
 def plot_heatmap(data, 
-                xlabel,
-                ylabel,                                   
+                ylabel,
+                xlabel,                                   
                 minimization=False,
                 savefig_path=None,
                  ):
     plt.clf()
     plt.cla()
 
-    ax = sns.heatmap(data)
+    ser = pd.Series(list(data.values()),
+                  index=pd.MultiIndex.from_tuples(data.keys()))
+    df = ser.unstack().fillna(0)
+    df = ser.unstack().fillna(np.inf)
+
+    # figure
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    cmap = sns.cubehelix_palette(as_cmap=True)
+
+    # Set the color for the under the limit to be white (0.0) so empty cells are not visualized
+    # cmap.set_under('-1.0')
+    # Plot NaN in white
+    cmap.set_bad(color='white')    
+    
+    ax = sns.heatmap(df)
     ax.invert_yaxis()
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
-     # get figure to save to file
+    # get figure to save to file
     if savefig_path:
-         ht_figure = ax.get_figure()
-         ht_figure.savefig(savefig_path+"/heatmap_"+xlabel+"_"+ylabel, dpi=400)
+        ht_figure = ax.get_figure()
+        fig_name = savefig_path+"/heatmap_"+xlabel+"_"+ylabel
+        print(os.path.abspath(fig_name))
+        ht_figure.savefig(fig_name)
+
 
     plt.clf()
     plt.cla()
     plt.close()
+ 
     
 
 def plot_svg(xml, filename):
